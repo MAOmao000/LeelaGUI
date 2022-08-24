faaeb81f8c995b3c7321831b0a81f17d0e2e4b78
0869x <0869xsjq@jcom.zaq.ne.jp>
2022-08-24 16:08:38 +0900

    Change the include file <CL/cl2.hpp> to <CL/opencl.hpp>.

--- engine/OpenCL.h
+++ engine/OpenCL.h
@@ -6,7 +6,7 @@
 #define CL_HPP_MINIMUM_OPENCL_VERSION   110
 #define CL_HPP_TARGET_OPENCL_VERSION    120
 #define CL_HPP_ENABLE_EXCEPTIONS
-#include <CL/cl2.hpp>
+#include <CL/opencl.hpp> // <CL/cl2.hpp>
 
 #include <atomic>
 #include <string>
