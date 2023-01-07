9f58c6330e6cd060aae5d694b7d66fef80c00b6d
0869x <0869xsjq@jcom.zaq.ne.jp>
2023-01-08 01:14:09 +0900

    Changed think time for KataGo GTP engine.

--- MainFrame.cpp
+++ MainFrame.cpp
@@ -1012,7 +1012,7 @@ void MainFrame::startKataGo() {
                 m_katagoStatus = KATAGO_GTP_ANALYSIS;
             } else {
                 m_StateEngine->start_clock(color);
-                m_gtp_send_cmd = wxString::Format("time_settings 0 %i 1\n", time_for_move - 1);
+                m_gtp_send_cmd = wxString::Format("time_settings 0 %i 1\n", time_for_move);
                 m_out->Write(m_gtp_send_cmd, strlen(m_gtp_send_cmd));
                 m_runflag = true;
                 m_katagoStatus = KATAGO_GTP_WAIT;
